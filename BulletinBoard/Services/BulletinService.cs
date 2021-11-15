e7d720d61d7b4a3a6dbf186977813a684368fb05
P8sx <hubgit@phsx.pl>
2021-11-15 16:35:34 +0100

    groupuser added

--- BulletinBoard/Services/BulletinService.cs
+++ BulletinBoard/Services/BulletinService.cs
@@ -7,16 +7,27 @@ namespace BulletinBoard.Services
     public class BulletinService : IBulletinService
     {
         private readonly ApplicationDbContext _dbContext;
+        private readonly ILogger _logger;
 
-        public BulletinService(ApplicationDbContext dbContext)
+        public BulletinService(ApplicationDbContext dbContext, ILogger<BulletinService> logger)
         {
-            this._dbContext = dbContext;
+            _dbContext = dbContext;
+            _logger = logger;
         }
 
         public async Task<bool> AddBulletin(Bulletin bulletin)
         {
-            await _dbContext.Bulletins.AddAsync(bulletin);
-            await _dbContext.SaveChangesAsync();
+            try
+            {
+                await _dbContext.Bulletins.AddAsync(bulletin);
+                await _dbContext.SaveChangesAsync();
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return false;
+            }
             return true;
         }
         public async Task<IList<Bulletin>> GetBulletinsAsync(int page, int limit)
