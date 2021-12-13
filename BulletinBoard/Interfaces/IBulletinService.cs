c85322b54fe4eb1a6b5d355229aa357b7f81a839
P8sx <ry@phsx.pl>
2021-12-13 17:09:50 +0100

    pagination count

--- BulletinBoard/Interfaces/IBulletinService.cs
+++ BulletinBoard/Interfaces/IBulletinService.cs
@@ -9,8 +9,8 @@ namespace BulletinBoard.Services
 
         Task<IList<BulletinInfoDTO>> GetBulletinsAsyncCached(int page, int limit, User user, Group group, BulletinSort sort = default);
         Task<IList<BulletinInfoDTO>> GetBulletinsAsyncCached(int page, int limit, User user, BulletinSort sort = default);
-
-
+        Task<int> GetBulletinsCountAsyncCached(User user, Group group);
+        Task<int> GetBulletinsCountAsyncCached(User user);
         Task Vote(BulletinVote vote);
     }
 }
